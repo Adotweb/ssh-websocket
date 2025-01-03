ac84c03e219ef2f9a75a776ffd5c3b340152ca9a
Alim Weber <alim.weber@outlook.com>
2025-01-03 16:55:27 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 const express = require("express");
 
-const {WebSocketServer} = require("ws");
+const app = express();
 
+const server = require("http").createServer(app)
+const { WebSocketServer } = require("ws")
+
+const wss = new WebSocketServer({server, path : "/term"})
 
 const {exec} = require("child_process");
 
-const app = express();
 
-
-const server = require("http").createServer(app);
-
-app.use(express.static(__dirname + "/public/"));
-
-
-const wss = new WebSocketServer({server, path : "/term"});
+app.use(express.static(__dirname + "/public/"))
 
 
 wss.on("connection", socket => {
+
+	socket.on("message", (proto) => {
+
+		const input = proto.toString();
 	
-	socket.on("message", proto => {
-		
-		let msg = proto.toString();
 
-		exec(msg, (err, out, stderr) => {
-
+		exec(input, (err, stdout, stderr) => {
+			
 			if(err){
-				socket.send(JSON.stringify({type : "error", msg : err.message}));
-				return;
-			}	
-			if(stderr){
-				socket.send(JSON.stringify({type : "error", msg : stderr}));
-				return;
+				socket.send(JSON.stringify({type : "error", msg : err.message}))
+				return
+			}if(stderr){
+				socket.send(JSON.stringify({type : "error", msg : stderr}))
+				return
 			}
+			socket.send(JSON.stringify({type : "out", msg : stdout}))
+		})
 
-			socket.send(JSON.stringify({type : "none", msg : out}));
-		});
+	})
 
-	});
+})
 
-});
-
-server.listen(3000);
+server.listen(3000)
